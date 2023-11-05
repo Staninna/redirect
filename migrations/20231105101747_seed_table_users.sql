fcd781f67baf1e3a585c1f894ffe96ff0cc9fe0f
Staninna <26458805+Staninna@users.noreply.github.com>
2023-11-05 15:04:13 +0100

    Updated migrations

---
+++ migrations/20231105101747_seed_table_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, email, password)
+VALUES
+('admin', 'admin@admin.com', '$2a$12$e/0u0dWnWdUVEerPcla.IOai.NqPBdW0XdDHDBMeoC7bhyLTi5QGC' -- password: admin
+),
+('user', 'user@user.com', '$2a$12$F5HBO9RQzD.P0wT1U7zHxu243DwMDrSfljdsAdkdYgG7qBBL6GNcq' -- password: user
+);
